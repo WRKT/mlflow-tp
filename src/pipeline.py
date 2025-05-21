88ea6f0597cbf0147d1a3f64e217269eb63c0b4b
Winness R. <winness.rakotozafy@outlook.fr>
2025-05-21 13:16:19 +0200

    feat: add tests ci

---
+++ src/pipeline.py
@@ -0,0 +1,16 @@
+import mlflow
+from ultralytics import YOLO
+
+def train_demo():
+    with mlflow.start_run(run_name="demo", log_system_metrics=True) as run:
+        model = YOLO("yolo11n.pt")
+        model.train(data="coco8.yaml", epochs=2)
+        mlflow.log_artifact(
+            local_path="requirements.txt",
+            artifact_path="environment",
+            run_id=run.info.run_id,
+        )
+
+
+if __name__ == "__main__":
+    train_demo()
